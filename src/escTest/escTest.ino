b63810453d4aaa07d9294540aaae7451e1de19d2
ilyas121 <ilyassalhi@gmail.com>
2019-04-11 14:15:48 -0400

    bug fix

--- src/escTest/escTest.ino
+++ src/escTest/escTest.ino
@@ -1,28 +1,6 @@
 #include <ESP32Servo.h>
 #include "config.h"
 #include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
-
-/* This driver reads raw data from the BNO055
-
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3.3V DC
-   Connect GROUND to common ground
-
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-*/
-
-/* Set the delay between fresh samples */
-#define BNO055_SAMPLERATE_DELAY_MS (100)
-
-Adafruit_BNO055 bno = Adafruit_BNO055();
 // ---------------------------------------------------------------------------
 Servo motA, motB, motC, motD;
 String data;
@@ -38,28 +16,6 @@ void setup() {
     motC.attach(LOWER_LEFT_MOTOR, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
     motD.attach(LOWER_RIGHT_MOTOR, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
     giveInstructions();
-    Serial.println("Orientation Sensor Raw Data Test"); Serial.println("");
-
-     /* Initialise the sensor */
-    if(!bno.begin())
-    {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-    }
-
-  delay(1000);
-
-  /* Display the current temperature */
-  int8_t temp = bno.getTemp();
-  Serial.print("Current Temperature: ");
-  Serial.print(temp);
-  Serial.println(" C");
-  Serial.println("");
-
-  bno.setExtCrystalUse(true);
-
-  Serial.println("Calibration status values: 0=uncalibrated, 3=fully calibrated");
 }
 
 
@@ -88,33 +44,7 @@ void loop() {
             maxAcc = 0;
         }
     }
-      // Possible vector values can be:
-  // - VECTOR_ACCELEROMETER - m/s^2
-  // - VECTOR_MAGNETOMETER  - uT
-  // - VECTOR_GYROSCOPE     - rad/s
-  // - VECTOR_EULER         - degrees
-  // - VECTOR_LINEARACCEL   - m/s^2
-  // - VECTOR_GRAVITY       - m/s^2
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
 
-  /* Display the floating point data */
-  double x = euler.x();
-  double y = euler.y();
-  double z = euler.z();
-
-  if(x < 0){
-    x *= -1;
-  }
-  if(y < 0){
-    y*= -1;
-  }
-  if(z < 0){
-    z*=-1;
-  }
-  if((x+y+z) > maxAcc){
-    maxAcc = x+y+z;
-  }
-  Serial.println("Max = " + String(maxAcc));
 }
 
 /**
