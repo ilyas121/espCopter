f2dcff5a7ee38d66e45d4cd90063edd423f987e9
Ilyas Salhi <ilyassalhi@gmail.com>
2020-04-03 23:59:34 -0400

    Simple python script for responding to UDP requests

--- missionControl.py
+++ missionControl.py
@@ -1,9 +1,31 @@
+# This python script listens on UDP port 1234 
+# for messages from the ESP32 board and prints them
 import socket
+import sys
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-client.bind(("", 37020))
-while True:
-    print("getting data")
-    data, addr = client.recvfrom(1024)
-    print("received message: %s"%data)
+try :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+except socket.error, msg :
+    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+try:
+    s.bind(('', 1234))
+except socket.error , msg:
+    print 'Bind failed. Error: ' + str(msg[0]) + ': ' + msg[1]
+    sys.exit()
+
+print 'Server listening'
+
+while 1:
+    d = s.recvfrom(1024)
+    data = d[0]
+
+    if not data:
+        break
+
+    print data.strip()
+    sendBoi = s.sendto("K".encode(), d[1])
+
+s.close()
