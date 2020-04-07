3f888aef766fe66cac59d6f47452165f22c3568d
Ilyas Salhi <ilyassalhi@gmail.com>
2020-04-07 00:09:27 -0400

    Fixed dependency management, added OTA just cause

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Arduino.h"
 #include <ESP32Servo.h>
 #include "config.h"
+#include "Secrets.h"
 #include "Reciever.h"
 #include "MotorController.h"
 #include "Imu.h"
@@ -9,7 +10,9 @@
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include "ArduinoOTA.h"
-
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <Update.h>
 // ---------------------------------------------------------------------------
 //Globals
 
@@ -57,8 +60,8 @@ Servo motors[4];
 // ---------------------------------------------------------------------------
 
 
-//----------------------------------------------------------------------------
-//ISR Routines
+// -------------------------------------------------------------------------
+// ISR Routines
 void changeRh(){
   portENTER_CRITICAL_ISR(&mux);
   double temp = esp_timer_get_time()-pastRh;
@@ -145,7 +148,60 @@ void changeKr(){
 void setup() {
     Serial.begin(115200);
     Serial.println("Booting");
-  
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(MY_SSID, MY_PASSWORD);
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.println("Connection Failed! Rebooting...");
+      delay(5000);
+      ESP.restart();
+    }
+
+    // Port defaults to 3232
+    // ArduinoOTA.setPort(3232);
+
+    // Hostname defaults to esp3232-[MAC]
+    // ArduinoOTA.setHostname("myesp32");
+
+    // No authentication by default
+    // ArduinoOTA.setPassword("admin");
+
+    // Password can be set with it's md5 value as well
+    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+    ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+          type = "sketch";
+        else // U_SPIFFS
+          type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() {
+        Serial.println("\nEnd");
+      })
+      .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+      })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+      });
+
+    ArduinoOTA.begin();
+
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    //////////////////////////
+    ///Setup motors
     motA.attach(UPPER_LEFT_MOTOR, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
     motB.attach(UPPER_RIGHT_MOTOR, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
     motC.attach(LOWER_LEFT_MOTOR, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
@@ -179,6 +235,7 @@ void setup() {
  * Loop: Read input and execute instruction
  */
 void loop() {
+    ArduinoOTA.handle();
     switch(droneState){
         case(HomeAllSwitches):
             if(valueKl < 1400 || valueKr < 1400){
