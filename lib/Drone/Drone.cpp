d217e68bf18fbb1da46df874585c412fb48b466f
Ilyas Salhi <ilyassalhi@gmail.com>
2020-05-23 16:23:27 -0400

    Refactored code to helper functions

--- lib/Drone/Drone.cpp
+++ lib/Drone/Drone.cpp
@@ -1,10 +1,12 @@
 #include "Drone.h"
 
+
+//Main loop that gets called outside of the class
 void Drone::loop() {
 	if(started == true){
 	   if(USE_IMU == true){
 		sensor->loop();
-           }   
+        }   
 	   sensor->getData(imuValues);
 	   Serial.println("Euler: " + String(imuValues[9]) + ", "  + String(imuValues[10]) + ", " +  String(imuValues[11]));
 	   delay(100);
@@ -19,6 +21,7 @@ void Drone::loop() {
 	//Serial.println("Pid X Output: " + String(velControlX));
 }
 
+//Constructor for when an object is created
 Drone::Drone(MotorController* mc, Reciever* r){ 
 	sensor = NULL;
 	controller = mc;
@@ -26,8 +29,32 @@ Drone::Drone(MotorController* mc, Reciever* r){
 	setup();
 }
 
+//Setup function called in the constructor
 void Drone::setup() {
-        if(USE_IMU == true){ 
+    if(USE_IMU == true){ 
+		imuSetup();
+	}
+	pidSetup();
+
+	//This attaches the output of the pid controller to the motor controller
+	controller->attachControllers(output);
+	started = true;
+}
+
+//Setup helper function for instantiating the pid controllers
+void Drone::pidSetup(){
+	//Roll = 1 Pitch = 2 Yaw = 0
+	velControllers[0] = new DPID(&imuValues[9], &velControlX, xK[0], xK[1], xK[2]);
+	velControllers[1] = new DPID(&imuValues[10], &velControlY, yK[0], yK[1], yK[2]);
+	velControllers[2] = new DPID(&imuValues[11], &velControlZ, zK[0], zK[1], yK[2]);
+	
+	posControllers[0] = new DPID(&imuValues[9], &velSetpoints[0], xK[0], xK[1], xK[2]);
+	posControllers[1] = new DPID(&imuValues[10], &velSetpoints[1], yK[0], yK[1], yK[2]);
+	posControllers[2] = new DPID(&imuValues[11], &velSetpoints[2], zK[0], zK[1], yK[2]);
+}
+
+//Setup helper function for the IMU
+void Drone::imuSetup(){
 	sensor = new Imu();
 	/* Initialise the sensor */
 	Serial.println("Setting up drone and sensors");	
@@ -45,48 +72,12 @@ void Drone::setup() {
 	delay(1000);
 	bno.setExtCrystalUse(true);
 	sensor->startSensor(&bno);
-	}
-	started = true;
-
-	velControlX = 0;
-	velControlY = 0;
-	velControlZ = 0;
-
-	//Roll = 1 Pitch = 2 Yaw = 0
-	velControllers[0] = new DPID(&imuValues[9], &velControlX, xK[0], xK[1], xK[2]);
-	velControllers[1] = new DPID(&imuValues[10], &velControlY, yK[0], yK[1], yK[2]);
-	velControllers[2] = new DPID(&imuValues[11], &velControlZ, zK[0], zK[1], yK[2]);
-	
-	posControllers[0] = new DPID(&imuValues[9], &velSetpoints[0], xK[0], xK[1], xK[2]);
-	posControllers[1] = new DPID(&imuValues[10], &velSetpoints[1], yK[0], yK[1], yK[2]);
-	posControllers[2] = new DPID(&imuValues[11], &velSetpoints[2], zK[0], zK[1], yK[2]);
-
-
-	
-	controller->attachControllers(output);
 }
 
+//Sub loop called from the main Drone::loop
 void Drone::fastLoop() { 
-//	Serial.println("Getting Data");
-	rc->getData(rcValues);
-/**	Serial.println("Done getting data");
-	Serial.print("Values: ");
-	Serial.print(rcValues[0]);
-	Serial.print(", ");
-	Serial.print(rcValues[1]);
-	Serial.print(", ");
-	Serial.print(rcValues[2]);
-	Serial.print(", ");
-	Serial.print(rcValues[3]);
-	Serial.println(".");**/
-	//Yaw = 0 - Roll = 2 - Pitch = 3  
-	rcValues[0] -= 1500;
-	rcValues[2] -= 1500;
-	rcValues[3] -= 1500;
-
-	rcValues[0] /= 3;
-	rcValues[2] /= 3;
-	rcValues[3] /= 3; 
+    //Get values from the reviever/remote controller 
+    getHumanInput();
 /**	
 	//Calculating required velocity
 	posControllers[0]->calculate();
@@ -113,7 +104,6 @@ void Drone::fastLoop() {
         output[2] = rcValues[1] - velControlY; 
         output[3] = rcValues[1] + velControlY;
     }
-	controller->loop();
    /** 
 	output[0] = rcValues[1] - velControlY - velControlZ + velControlX; 
 	output[1] = rcValues[1] + velControlY - velControlZ - velControlX; 
@@ -129,4 +119,29 @@ void Drone::fastLoop() {
 	output[1] = rcValues[1] - pid_output_pitch + pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 1 (front-right - CCW)
 	output[2] = rcValues[1] + pid_output_pitch - pid_output_roll - pid_output_yaw; //Calculate the pulse for esc 3 (rear-left - CCW)
 	output[3] = rcValues[1] + pid_output_pitch + pid_output_roll + pid_output_yaw; //Calculate the pulse for esc 2 (rear-right - CW)**/
+	controller->loop();
+}
+
+
+//Takes the raw RC data and converts it for drone input
+void Drone::getHumanInput(){
+	rc->getData(rcValues);
+/**	Serial.println("Done getting data");
+	Serial.print("Values: ");
+	Serial.print(rcValues[0]);
+	Serial.print(", ");
+	Serial.print(rcValues[1]);
+	Serial.print(", ");
+	Serial.print(rcValues[2]);
+	Serial.print(", ");
+	Serial.print(rcValues[3]);
+	Serial.println(".");**/
+	//Yaw = 0 - Roll = 2 - Pitch = 3  
+	rcValues[0] -= 1500;
+	rcValues[2] -= 1500;
+	rcValues[3] -= 1500;
+
+	rcValues[0] /= 3;
+	rcValues[2] /= 3;
+	rcValues[3] /= 3; 
 }
--- lib/Drone/Drone.h
+++ lib/Drone/Drone.h
@@ -15,13 +15,15 @@ class Drone {
 private:
 	Imu* sensor;
 	Adafruit_BNO055 bno;
-        MotorController* controller;	
+    MotorController* controller;	
 	Reciever* rc; 
-        double started = false;
-
+    
+	double started = false;
 	double rcValues[6];
 	double imuValues[12];
-	double velControlX, velControlY, velControlZ;
+	double velControlX = 0;
+	double velControlY = 0;
+	double velControlZ = 0;
 	double output[4] = {0, 0, 0, 0};
 	double xK[3] = {0, 0, 0};
 	double yK[3] = {12, 0.00, 0};
@@ -31,10 +33,13 @@ private:
 	DPID* posControllers[3];
 
 	void printAll();
-	// This should be run every loop and is internally gated for fast opperation
 	void fastLoop();
 	// Internal setup function. set up all objects
 	void setup();
+	void imuSetup();
+	void pidSetup();
+	void getHumanInput();
+
 public:
 	Drone(MotorController* mc, Reciever* r);
 	// Pulse the loop function from the main thread
